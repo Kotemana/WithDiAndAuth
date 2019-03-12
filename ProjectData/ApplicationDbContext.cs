7d3a57d9ce47a3628c43e42d5c187414ae6bebac
Leonid Radkevich <laradkevich@gmail.com>
2019-03-12 22:38:22 +0300

    fix

---
+++ ProjectData/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using ProjectData.Identity;
+
+namespace ProjectData
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
